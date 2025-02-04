2c1c438f808a49fa69c6a53473a0656e6ac2eed5
Henry Catalini Smith <henry@catalinismith.com>
2025-02-04 07:50:50 +0100

    Set up front page:

--- app/page.tsx
+++ app/page.tsx
@@ -5,35 +5,65 @@ import {
   BreadcrumbList,
   BreadcrumbSeparator,
 } from "@arbetsmarknad/components/Breadcrumb";
+import {
+  DescriptionList,
+  DescriptionItem,
+  DescriptionTerm,
+  DescriptionDetails,
+} from "@arbetsmarknad/components/DescriptionList";
 import { Container } from "@arbetsmarknad/components/Container";
 import { HeaderMenu } from "@arbetsmarknad/components/HeaderMenu";
 import { Page } from "@arbetsmarknad/components/Page";
+import { Section } from "@arbetsmarknad/components/Section";
+import { SectionHeading } from "@arbetsmarknad/components/SectionHeading";
 import { TopLevelHeading } from "@arbetsmarknad/components/TopLevelHeading";
+
+const yearsWithData = ["2024", "2023"];
 
 export default function Home() {
   return (
     <Page>
       <HeaderMenu
-        href="https://arbetsmarknad.github.io"
-        text="arbetsmarknad.github.io"
+        href="https://arbetsmiljo.github.io"
+        text="arbetsmiljo.github.io"
       />
       <Breadcrumb className="py-4 w-full flex justify-center">
         <Container>
           <BreadcrumbList>
             <BreadcrumbItem>
-              <BreadcrumbLink href="/">Arbetsmarknad</BreadcrumbLink>
+              <BreadcrumbLink href="https://arbetsmarknad.github.io">
+                Arbetsmarknad
+              </BreadcrumbLink>
             </BreadcrumbItem>
             <BreadcrumbSeparator />
             <BreadcrumbItem>
-              <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+              <BreadcrumbLink href="/">Arbetsmiljö</BreadcrumbLink>
             </BreadcrumbItem>
           </BreadcrumbList>
         </Container>
       </Breadcrumb>
       <main className="flex flex-col items-center w-full py-4">
         <Container className="flex flex-col items-start space-y-4">
-          <TopLevelHeading text="Arbetsmarknad Starter Project" />
-          <p>This is a starter project for Arbetsmarknad web content.</p>
+          <TopLevelHeading text="Arbetsmiljödata och -statistik" />
+          <p></p>
+          <Section>
+            <SectionHeading>År</SectionHeading>
+            <DescriptionList>
+              {yearsWithData.map((year) => (
+                <DescriptionItem key={year}>
+                  <DescriptionTerm>
+                    <a
+                      className="font-bold underline text-blue-600"
+                      href={`/${year}`}
+                    >
+                      {year}
+                    </a>
+                  </DescriptionTerm>
+                  <DescriptionDetails>{`Statistik om arbetsmiljön ${year}`}</DescriptionDetails>
+                </DescriptionItem>
+              ))}
+            </DescriptionList>
+          </Section>
         </Container>
       </main>
     </Page>
